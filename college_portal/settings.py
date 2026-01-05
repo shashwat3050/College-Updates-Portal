abecc9d2493645ba68dba8a7fd622347bfa7cb52
Shashwat Sharma <shashwat3050@gmail.com>
2026-01-05 14:45:48 +0530

    Collect static files for Railway

--- college_portal/settings.py
+++ college_portal/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
